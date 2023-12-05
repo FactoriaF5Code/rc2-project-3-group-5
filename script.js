3ee45296adbe32514f81caf8916be3a6384445a9
Iciar Rodríguez <iciar@MacBook-Pro-de-Iciar.local>
2023-12-05 12:09:21 +0100

    Creados todos los sonidos de la bateria con java

---
+++ script.js
@@ -0,0 +1,47 @@
+function REPplatilloIzquierdoAlto() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOplatilloIzquierdoAlto")
+
+    SONIDOplatilloIzquierdoAlto.play(); 
+}
+
+function REPplatilloIzquierdoBajo() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOplatilloIzquierdoBajo")
+
+    SONIDOplatilloIzquierdoBajo.play(); 
+}
+
+function REPplatilloDerecho() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOplatilloDerecho")
+
+    SONIDOplatilloDerecho.play(); 
+}
+
+function REPtamborIzquierdo() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOtamborIzquierdo")
+
+    SONIDOtamborIzquierdo.play(); 
+}
+
+function REPbombo() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDObombo")
+
+    SONIDObombo.play(); 
+}
+
+function REPtamborDerecho() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOtamborDerecho")
+
+    SONIDOtamborDerecho.play(); 
+}
+
+function REPizquierdoSuperior() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOizquierdoSuperior")
+
+    SONIDOizquierdoSuperior.play(); 
+}
+
+function REPderechoSuperior() {
+    let platilloIzquierdoAlto = document.getElementById("SONIDOderechoSuperior")
+
+    SONIDOderechoSuperior.play(); 
+}
