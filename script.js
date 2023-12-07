1cdfe135bdd28ccfd94d8f9a58e3999ee9adae5a
VicSDN <151613334+VicSDN@users.noreply.github.com>
2023-12-07 15:33:44 +0100

    Finalizado resplandor de tecla

--- script.js
+++ script.js
@@ -3,139 +3,196 @@ customCursor.id = "customCursor";
 document.body.appendChild(customCursor);
 
 document.addEventListener("mousemove", (e) => {
-    customCursor.style.left = e.pageX - 25 + "px";
-    customCursor.style.top = e.pageY - 25 + "px";
+  customCursor.style.left = e.pageX - 25 + "px";
+  customCursor.style.top = e.pageY - 25 + "px";
 });
 
 function tocarPlatilloIzquierdoAlto() {
-    let platilloIzquierdoAlto = document.getElementById("sonidoPlatilloIzquierdoAlto");
-    let sombra = document.getElementById("s"); 
-    
-    
-    sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
-    
-    setTimeout(function() {
-        sombra.style.boxShadow = "none"; 
-    }, 200); 
-    
-    if (platilloIzquierdoAlto.paused) { 
-        platilloIzquierdoAlto.play();
-    } else {
-        platilloIzquierdoAlto.currentTime = 0;
-    }
-}
+  let platilloIzquierdoAlto = document.getElementById(
+    "sonidoPlatilloIzquierdoAlto"
+  );
+  let sombra = document.getElementById("s");
 
-document.addEventListener("keydown", function(event) {
-    if (event.key === "s") {
-        tocarPlatilloIzquierdoAlto();
-    }
-});
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
 
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
 
-
-function tocarPlatilloIzquierdoBajo() {
-    let platilloIzquierdoBajo = document.getElementById("sonidoPlatilloIzquierdoBajo")
-    if (platilloIzquierdoBajo.paused) {
-        platilloIzquierdoBajo.play();
-    } else {
-        platilloIzquierdoBajo.currentTime = 0;
-    }
+  if (platilloIzquierdoAlto.paused) {
+    platilloIzquierdoAlto.play();
+  } else {
+    platilloIzquierdoAlto.currentTime = 0;
+  }
 }
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === "d") {
-        tocarPlatilloIzquierdoBajo();
-    }
+  if (event.key === "s") {
+    tocarPlatilloIzquierdoAlto();
+  }
+});
+
+function tocarPlatilloIzquierdoBajo() {
+  let platilloIzquierdoBajo = document.getElementById(
+    "sonidoPlatilloIzquierdoBajo"
+  );
+  let sombra = document.getElementById("d");
+
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (platilloIzquierdoBajo.paused) {
+    platilloIzquierdoBajo.play();
+  } else {
+    platilloIzquierdoBajo.currentTime = 0;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "d") {
+    tocarPlatilloIzquierdoBajo();
+  }
 });
 
 function tocarPlatilloDerecho() {
-    let platilloDerecho = document.getElementById("sonidoPlatilloDerecho")
-    if (platilloDerecho.paused) {
-        platilloDerecho.play();
-    } else {
-        platilloDerecho.currentTime = 0;
-    }
-}
+  let platilloDerecho = document.getElementById("sonidoPlatilloDerecho");
+  let sombra = document.getElementById("l");
 
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (platilloDerecho.paused) {
+    platilloDerecho.play();
+  } else {
+    platilloDerecho.currentTime = 0;
+  }
+}
 document.addEventListener("keydown", function (event) {
-    if (event.key === "l") {
-        tocarPlatilloDerecho();
-    }
+  if (event.key === "l") {
+    tocarPlatilloDerecho();
+  }
 });
 
 function tocarTamborIzquierdo() {
-    let tamborIzquierdo = document.getElementById("sonidoTamborIzquierdo")
-    if (tamborIzquierdo.paused) {
-        tamborIzquierdo.play();
-    } else {
-        tamborIzquierdo.currentTime = 0;
-    }
+  let tamborIzquierdo = document.getElementById("sonidoTamborIzquierdo");
+  let sombra = document.getElementById("f");
+
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (tamborIzquierdo.paused) {
+    tamborIzquierdo.play();
+  } else {
+    tamborIzquierdo.currentTime = 0;
+  }
 }
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === "f") {
-        tocarTamborIzquierdo();
-    }
+  if (event.key === "f") {
+    tocarTamborIzquierdo();
+  }
 });
 
 function tocarBombo() {
-    let bombo = document.getElementById("sonidoBombo")
-    if (bombo.paused) {
-        bombo.play();
-    } else {
-        bombo.currentTime = 0;
-    }
+  let tocarBombo = document.getElementById("sonidoBombo");
+  let sombra = document.getElementById("h");
+
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (tocarBombo.paused) {
+    tocarBombo.play();
+  } else {
+    tocarBombo.currentTime = 0;
+  }
 }
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === "h") {
-        tocarBombo();
-    }
+  if (event.key === "h") {
+    tocarBombo();
+  }
 });
 
 function tocarTamborDerecho() {
-    let tamborDerecho = document.getElementById("sonidoTamborDerecho")
-    if (tamborDerecho.paused) {
-        tamborDerecho.play();
-    } else {
-        tamborDerecho.currentTime = 0;
-    }
+  let tocarTamborDerecho = document.getElementById("sonidoTamborDerecho");
+  let sombra = document.getElementById("k");
+
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (tocarTamborDerecho.paused) {
+    tocarTamborDerecho.play();
+  } else {
+    tocarTamborDerecho.currentTime = 0;
+  }
 }
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === "k") {
-        tocarTamborDerecho();
-    }
+  if (event.key === "k") {
+    tocarTamborDerecho();
+  }
 });
 
 function tocarIzquierdoSuperior() {
-    let izquierdoSuperior = document.getElementById("sonidoIzquierdoSuperior")
-    if (izquierdoSuperior.paused) {
-        izquierdoSuperior.play();
-    } else {
-        izquierdoSuperior.currentTime = 0;
-    }
-}
+  let tocarIzquierdoSuperior = document.getElementById(
+    "sonidoIzquierdoSuperior"
+  );
+  let sombra = document.getElementById("g");
 
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (tocarIzquierdoSuperior.paused) {
+    tocarIzquierdoSuperior.play();
+  } else {
+    tocarIzquierdoSuperior.currentTime = 0;
+  }
+}
 document.addEventListener("keydown", function (event) {
-    if (event.key === "g") {
-        tocarIzquierdoSuperior();
-    }
+  if (event.key === "g") {
+    tocarIzquierdoSuperior();
+  }
 });
 
 function tocarDerechoSuperior() {
-    let derechoSuperior = document.getElementById("sonidoDerechoSuperior")
-    if (derechoSuperior.paused) {
-        derechoSuperior.play();
-    } else {
-        derechoSuperior.currentTime = 0;
-    }
+  let tocarDerechoSuperior = document.getElementById(
+    "sonidoDerechoSuperior"
+  );
+  let sombra = document.getElementById("j");
+
+  sombra.style.boxShadow = "0px 0px 7px 8px rgba(252, 245, 245, 0.5)";
+
+  setTimeout(function () {
+    sombra.style.boxShadow = "none";
+  }, 200);
+
+  if (tocarDerechoSuperior.paused) {
+    tocarDerechoSuperior.play();
+  } else {
+    tocarDerechoSuperior.currentTime = 0;
+  }
 }
 
 document.addEventListener("keydown", function (event) {
-    if (event.key === "j") {
-        tocarDerechoSuperior();
-    }
+  if (event.key === "j") {
+    tocarDerechoSuperior();
+  }
 });
-
-
